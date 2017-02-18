6ecf479b969affd29c8729ded6ac1d027c9624de
jp <gaines.johnp@gmail.com>
2017-02-18 14:05:27 -0800

    added async method

--- app.js
+++ app.js
@@ -2,6 +2,11 @@
 
 const fs = require('fs');
 
+//utf8 encoding specification returns the plain test.
 let greet = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
 console.log(greet);
 
+//without the encoding specification a buffer is returned
+let greet2 = fs.readFile(__dirname + '/greet.txt', (err, data)=> {
+  console.log(data);
+});
